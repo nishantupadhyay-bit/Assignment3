cea4d8279f1c32f6fb23044773022197b7385d6f
Nishant Upadhyay <nishant.upadhyay@gonuclei.com>
2026-09-25 15:59:05 +0530

    Assignment 3 - Add Entity for DB and its mapper

---
+++ src/model/ItemEntity.java
@@ -0,0 +1,61 @@
+package model;
+
+import enums.Type;
+import java.time.LocalDateTime;
+
+public class ItemEntity {
+    private String name;
+    private double price;
+    private int quantity;
+    private Type type;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
